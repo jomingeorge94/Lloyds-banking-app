aaa7c71ae5252c3df0adf6c473064ed6860f03b2
Dan Gardner <dan.gardner@live.co.uk>
2015-02-08 20:35:42 +0000

    Altered Main Activity
    Changed the MainActivity in the Master Branch to SplashActivity as
    Jomins project requires the MainActivity file for the tabs used in the
    home screen

--- Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/SplashActivity.java
+++ Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/SplashActivity.java
@@ -5,7 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.os.Handler;
 
-public class MainActivity extends Activity {
+public class SplashActivity extends Activity {
 
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 1000;
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
     @Override
     public void onCreate(Bundle icicle) {
         super.onCreate(icicle);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_splash);
 
         /* New Handler to start the Menu-Activity
          * and close this Splash-Screen after some seconds.*/
@@ -22,9 +22,9 @@ public class MainActivity extends Activity {
             @Override
             public void run() {
                 /* Create an Intent that will start the Menu-Activity. */
-                Intent mainIntent = new Intent(MainActivity.this, LoginActivity.class);
-                MainActivity.this.startActivity(mainIntent);
-                MainActivity.this.finish();
+                Intent mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                SplashActivity.this.startActivity(mainIntent);
+                SplashActivity.this.finish();
             }
         }, SPLASH_DISPLAY_LENGTH);
 
