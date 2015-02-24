565c435a072c75d94da2da46ea037ab2543daa56
Dan Gardner <dan.gardner@live.co.uk>
2015-02-24 15:15:26 +0000

    Added Debug Start
    Added variables to the SpashActivity class which control the type of
    start you want. This enables the initial setup or the login to be
    skipped each time the app is run in order to help others when testing
    the app.
    
    There are three starts:
    - Normal Start which simply acts as if the user is opening the app, it
    will take you to the standard passcode login screen
    
    - First Time Start which makes the app act as if it is being setup for
    the first time ever
    
    - Skip Start which skips all of the login options and goes straight to
    the home tabs
    
    To use these options simply enable the one you want by changing its
    boolean value to true and all of the others to false

--- Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/SplashActivity.java
+++ Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/SplashActivity.java
@@ -10,6 +10,19 @@ public class SplashActivity extends Activity {
     /** Duration of wait **/
     private final int SPLASH_DISPLAY_LENGTH = 1000;
 
+    /*******
+     Debug variables, depending on which mode you want to be in alter the states of these variables.
+     One variable should be set to true - this should be the debug option you wish to enter - all
+     the others should be set to false
+     *******/
+
+    // Normal Start takes you to the login as if you are using the app as normal
+    private boolean normalStart = true;
+    // First Time Start takes you into the app as if you're a first time user
+    private boolean firstTimeStart = false;
+    // Skip Start takes you through to the home screen (Tabbed Screen)
+    private boolean skipStart = false;
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle icicle) {
@@ -20,8 +33,16 @@ public class SplashActivity extends Activity {
         new Handler().postDelayed(new Runnable(){
             @Override
             public void run() {
-                /* Create an Intent that will start the Login Screen */
-                Intent mainIntent = new Intent(SplashActivity.this, FirstLoginActivity.class);
+                Intent mainIntent;
+
+                if(normalStart == true) {
+                    mainIntent = new Intent(SplashActivity.this, LoginActivity.class);
+                } else if(firstTimeStart == true) {
+                    mainIntent = new Intent(SplashActivity.this, FirstLoginActivity.class);
+                } else {
+                    mainIntent = new Intent(SplashActivity.this, MainActivity.class);
+                }
+
                 SplashActivity.this.startActivity(mainIntent);
                 SplashActivity.this.finish();
             }
