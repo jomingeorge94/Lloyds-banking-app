501ad3d85f1e5318a4c2cc96b6808cc1744b6464
Dan Gardner <dan.gardner@live.co.uk>
2015-03-09 01:19:01 +0000

    Basic Dynamic Colours
    I have added a very basic style to the progress bar so it is now thicker
    and changes colour depending on the percentage of the budget used.
    
    Later I will try to change so rather than flicking between a set range
    of colours it will act even more dynamically.

--- Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/BudgetFragment.java
+++ Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/BudgetFragment.java
@@ -1,6 +1,8 @@
 package ncl.ac.uk.cs.teamone.lloydsstudent;
 
+import android.content.res.Resources;
 import android.graphics.Color;
+import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.util.Log;
@@ -21,6 +23,8 @@ public class BudgetFragment extends Fragment {
     private TextView total;
     private ProgressBar summary;
     private TextView rightLabel;
+    private Resources res;
+    Rect bounds;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -31,7 +35,7 @@ public class BudgetFragment extends Fragment {
         summary = (ProgressBar) v.findViewById(R.id.summary_bar);
         rightLabel = (TextView) v.findViewById(R.id.right_label);
 
-        budgetUpdate(88, 62.18);
+        budgetUpdate(88, 150);
 
         return v;
     }
@@ -42,6 +46,29 @@ public class BudgetFragment extends Fragment {
         rightLabel.setText("£" + String.format("%.2f", budget));
 
         summary.setMax((int) budget);
+        if(spent <= budget) {
+            summary.setProgress((int) spent);
+        } else {
+            summary.setProgress(100);
+        }
+
+        res = getResources();
+        bounds = summary.getProgressDrawable().getBounds();
+
+        if(spent >= budget) {
+            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_over));
+        }
+        else if (spent >= budget * 0.5) {
+            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_half));
+        }
+        else if (spent >= budget * 0.8) {
+            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_high));
+        }
+        else {
+            summary.setProgressDrawable(res.getDrawable(R.drawable.progress));
+        }
+
+        summary.getProgressDrawable().setBounds(bounds);
         summary.setProgress((int) spent);
     }
 
