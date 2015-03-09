c555807fd496c6fb791c317dac5d26fd137f8146
Dan Gardner <dan.gardner@live.co.uk>
2015-03-09 14:35:27 +0000

    Added Comments to Summary Code
    I've added comments to the code I've created for updating the summary
    view of the app

--- Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/BudgetFragment.java
+++ Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/BudgetFragment.java
@@ -1,14 +1,22 @@
 package ncl.ac.uk.cs.teamone.lloydsstudent;
 
+import android.content.Context;
 import android.content.res.Resources;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+
+import java.util.Random;
 
 /**
  * Created by Jomin on 26/02/2015.
@@ -19,7 +27,7 @@ public class BudgetFragment extends Fragment {
     private TextView total;
     private ProgressBar summary;
     private TextView rightLabel;
-    private Resources res;
+    private Resources resource;
     private Rect bounds;
     private TextView remaining;
 
@@ -33,57 +41,95 @@ public class BudgetFragment extends Fragment {
         rightLabel = (TextView) v.findViewById(R.id.right_label);
         remaining = (TextView) v.findViewById(R.id.budget_remaining);
 
-        budgetUpdate(88, 44.11);
-
+        summaryUpdate(88, 72.11);
+        
         return v;
     }
 
-    public void budgetUpdate(double budget, double spent) {
+    /**
+     * A method that updates the GUI summary sections. In order to do this it must know the
+     * budget of the user and the amount that has been spent in the current week, once it has
+     * these it updates any display elements so the show correctly
+     *
+     * @param budget the current weekly budget of the user
+     * @param spent the amount spent in the current week by the user
+     */
+    public void summaryUpdate(double budget, double spent) {
 
+        // Update text labels
         total.setText("£" + String.format("%.2f", budget));
         rightLabel.setText("£" + String.format("%.2f", budget));
         remaining.setText("£" + String.format("%.2f", budget - spent));
 
-        summary.setMax((int) budget);
-        if(spent <= budget) {
-            summary.setProgress((int) spent);
-        } else {
-            summary.setProgress(100);
-        }
-
-        res = getResources();
+        // Gets the resource files for the progress bar
+        resource = getResources();
         bounds = summary.getProgressDrawable().getBounds();
 
+        // GUI settings if amount spent is over budget
         if(spent >= budget) {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_over));
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_over));
             remaining.setTextColor(getResources().getColor(R.color.spend_over));
         }
+        // GUI settings if amount spent is over 90% of budget
         else if (spent >= budget * 0.9) {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_close));
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_close));
             remaining.setTextColor(getResources().getColor(R.color.spend_close));
         }
-        else if (spent >= budget * 0.8) {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_high));
+        // GUI settings if amount spent is over 70% of budget
+        else if (spent >= budget * 0.7) {
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_high));
             remaining.setTextColor(getResources().getColor(R.color.spend_high));
         }
+        // GUI settings if amount spent is over 50% of budget
         else if (spent >= budget * 0.5) {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_half));
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_half));
             remaining.setTextColor(getResources().getColor(R.color.spend_half));
         }
+        // GUI settings if amount spent is over 30% of budget
         else if (spent >= budget * 0.3) {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_low));
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_low));
             remaining.setTextColor(getResources().getColor(R.color.spend_low));
         }
+        // GUI settings if amount spent is under 30% of budget
         else {
-            summary.setProgressDrawable(res.getDrawable(R.drawable.progress_normal));
+            summary.setProgressDrawable(resource.getDrawable(R.drawable.progress_normal));
             remaining.setTextColor(getResources().getColor(R.color.dark_green));
         }
 
-        summary.getProgressDrawable().setBounds(bounds);
-        summary.setProgress((int) spent);
+        // Sets the range of the progress bar to the weekly budget
+        summary.setMax((int) budget);
+        // Sets the progress of the bar to the amount spent
+        if(spent <= budget) {
+            summary.setProgress((int) spent);
+            summary.getProgressDrawable().setBounds(bounds);
+        }
+        // If the amount spent is greater than the budget 100 is set for the progress
+        else {
+            summary.setProgress(100);
+            summary.getProgressDrawable().setBounds(bounds);
+        }
+
     }
 
+    public float[] generateChartData(float[] spend) {
 
+        // Temp value to store total spend
+        float total = 0;
+
+        // Sum of all the values in the array
+        for (float value : spend) {
+            total += value;
+        }
+
+        /* Works out the number of degrees to be assigned to arc by calculating the percentage
+        from the individual spend and the total */
+        for (float value : spend) {
+            value = 360 * (value / total);
+        }
+
+        // Returns an array containing the degree values for each arc
+        return spend;
+    }
 
 }
 
