adb28d55001a53354a21837ac758a68a527f4f8e
jomingeorge94 <j.george2@newcastle.ac.uk>
2015-03-10 15:02:34 +0000

    Drop down - two types of spinner mode
    spinner modes - dialog or drop down

--- Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/MakeaTransfer.java
+++ Application/app/src/main/java/ncl/ac/uk/cs/teamone/lloydsstudent/MakeaTransfer.java
@@ -23,13 +23,21 @@ public class MakeaTransfer extends Fragment implements AdapterView.OnItemSelecte
 
 
         spinner = (Spinner)v.findViewById(R.id.spinner);
-
         ArrayAdapter adapter = ArrayAdapter.createFromResource(getActivity(), R.array.accounts,R.layout.spinner_item);
         adapter.setDropDownViewResource(R.layout.spinner_dropdown_items);
-
-        spinner.setPrompt("Select the item");
         spinner.setAdapter(adapter);
+        spinner.setPrompt("Select an account");
         //spinner.setOnItemSelectedListener(this);
+
+        spinner = (Spinner)v.findViewById(R.id.spinner2);
+        ArrayAdapter adapter2 = ArrayAdapter.createFromResource(getActivity(), R.array.accounts,R.layout.spinner_item);
+        adapter.setDropDownViewResource(R.layout.spinner_dropdown_items);
+        spinner.setAdapter(adapter2);
+        spinner.setPrompt("Select an account");
+
+
+
+
 
 
 
