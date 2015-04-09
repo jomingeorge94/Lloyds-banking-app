60e68cef8f1639298ec2583593466149ef2bef47
boz95 <a.a.bozhenok@ncl.ac.uk>
2015-04-09 18:28:05 +0100

    Budget & Security Feature
    Implemented the _spend attributes into the app. The app will now either
    restart or kill itself depending on which activity the phone sleeps on

--- Application/app/src/main/php/retrieve_budget.php
+++ Application/app/src/main/php/retrieve_budget.php
@@ -5,7 +5,7 @@
 	$uid = $_POST['uid'];
 
 	if(isset($uid)) {
-		$array_of_data = fetch("Budget", "dateofbudget,amount,spend,travel_amount,beauty_and_hygiene_amount,entertainment_amount,other_amount,clothes_amount,leisurely_activities_amount,bills_amount,savings_amount,rent_amount,groceries_amount,eating_out_amount", $uid, BUDGET);
+		$array_of_data = fetch("Budget", "dateofbudget,amount,spend,travel_amount,beauty_and_hygiene_amount,entertainment_amount,home_amount,other_amount,clothes_amount,leisurely_activities_amount,bills_amount,savings_amount,rent_amount,groceries_amount,eating_out_amount,travel_spend,beauty_and_hygiene_spend,entertainment_spend,home_spend,other_spend,clothes_spend,leisurely_activities_spend,bills_spend,savings_spend,rent_spend,groceries_spend,eating_out_spend", $uid, BUDGET);
 
 		echo json_encode($array_of_data);
 	}
